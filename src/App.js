36fc236769d64427813a1e0b9f7fdeb0728a89f1
hugomarti <hgmarti@gmail.com>
2020-09-08 17:11:32 +0200

    implementing sign up with email and password

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sing-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./fiebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./fiebase/firebase.utils";
 
 import "./App.css";
 
@@ -17,8 +17,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = createUserProfileDocument(userAuth);
+
+        (await userRef).onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-buttom/custom-button.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import { signInWithGoogle } from "../../fiebase/firebase.utils";
 
